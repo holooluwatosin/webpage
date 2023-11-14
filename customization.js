fb9c3dbc0fc8261a5f32dab34202801740042b18
Holo Oluwatosin Kehinde <holooluwatosin@gmail.com>
2023-11-14 17:05:46 +0100

    setup of settings and index page

---
+++ customization.js
@@ -0,0 +1,91 @@
+// Load preferences from Local Storage
+document.addEventListener("DOMContentLoaded", function () {
+    loadPreferences();
+});
+
+function loadPreferences() {
+    var fontSize = localStorage.getItem("fontSize");
+    var bgColor = localStorage.getItem("bgColor");
+    var textColor = localStorage.getItem("textColor");
+
+    if (fontSize) {
+        document.getElementById("fontSize").value = fontSize;
+        changeFontSize();
+    }
+
+    if (bgColor) {
+        document.getElementById("bgColor").value = bgColor;
+        changeBackgroundColor();
+    }
+
+    if(textColor) {
+        document.getElementById("textColor").value = textColor;
+        changeTextColor();
+    }
+}
+
+function changeFontSize() {
+    var fontSizeValue = document.getElementById("fontSize").value;
+    document.getElementById("websiteHeader").style.fontSize = fontSizeValue + "em";
+    document.getElementById("websiteBody").style.fontSize = fontSizeValue + "em";
+
+    // Save preferences to Local Storage
+    localStorage.setItem("fontSize", fontSizeValue);
+}
+
+function changeBackgroundColor() {
+    var bgColorValue = document.getElementById("bgColor").value;
+    document.getElementById("websiteHeader").style.backgroundColor = bgColorValue;
+
+    // Save preferences to Local Storage
+    localStorage.setItem("bgColor", bgColorValue);
+}
+
+function changeTextColor() {
+    var textColorValue = document.getElementById("textColor").value;
+    document.getElementById("websiteHeader").style.color = textColorValue;
+
+    // Update the text color of the links in the navigation menu
+    var navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(function(link) {
+        link.style.color = textColorValue;
+    });
+
+    // Save preference to Local Storage
+    localStorage.setItem("textColor", textColorValue);
+}
+
+function resetPreferences() {
+    // Clear preferences from Local Storage
+    localStorage.removeItem("fontSize");
+    localStorage.removeItem("bgColor");
+    localStorage.removeItem("textColor")
+
+    // Reset styles to default
+    document.getElementById("websiteHeader").style.fontSize = "1em";
+    document.getElementById("websiteBody").style.fontSize = "1em";
+    document.getElementById("websiteHeader").style.backgroundColor = "#EAE3C9";
+    document.getElementById("websiteHeader").style.color = "#222222"
+
+    // Reset input values
+    document.getElementById("fontSize").value = 1;
+    document.getElementById("bgColor").value = "#EAE3C9";
+    document.getElementById("textColor").value = "#222222"
+}
+
+//Toggle function for mobile view
+function toggleMobileMenu() {
+    var mobileMenu = document.getElementById("mobileMenu");
+    var barsIcon = document.getElementById("barsIcon");
+    var timesIcon = document.getElementById("timesIcon");
+  
+    if (mobileMenu.style.display === "flex") {
+      mobileMenu.style.display = "none";
+      barsIcon.style.display = "inline";
+      timesIcon.style.display = "none";
+    } else {
+      mobileMenu.style.display = "flex";
+      barsIcon.style.display = "none";
+      timesIcon.style.display = "inline";
+    }
+}
