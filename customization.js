360f485cdbba5e920866a6b7551c8862e94ffdf2
Holo Oluwatosin Kehinde <holooluwatosin@gmail.com>
2023-11-17 12:16:45 +0100

    setup of footer section to suit the script functionalities

--- customization.js
+++ customization.js
@@ -28,6 +28,13 @@ function changeFontSize() {
     var fontSizeValue = document.getElementById("fontSize").value;
     document.getElementById("websiteHeader").style.fontSize = fontSizeValue + "em";
     document.getElementById("websiteBody").style.fontSize = fontSizeValue + "em";
+    // document.getElementById("websiteFooter").style.fontSize = fontSizeValue + "em";
+
+    // Update the font size for the footer
+    var footerElements = document.querySelectorAll('#websiteFooter *');
+    footerElements.forEach(function(element) {
+        element.style.fontSize = fontSizeValue + "em";
+    });
 
     // Save preferences to Local Storage
     localStorage.setItem("fontSize", fontSizeValue);
@@ -36,6 +43,7 @@ function changeFontSize() {
 function changeBackgroundColor() {
     var bgColorValue = document.getElementById("bgColor").value;
     document.getElementById("websiteHeader").style.backgroundColor = bgColorValue;
+    document.getElementById("websiteFooter").style.backgroundColor = bgColorValue;
 
     // Save preferences to Local Storage
     localStorage.setItem("bgColor", bgColorValue);
@@ -45,10 +53,21 @@ function changeTextColor() {
     var textColorValue = document.getElementById("textColor").value;
     document.getElementById("websiteHeader").style.color = textColorValue;
 
+    
     // Update the text color of the links in the navigation menu
     var navLinks = document.querySelectorAll('.nav-link');
     navLinks.forEach(function(link) {
         link.style.color = textColorValue;
+    });
+    
+    // Update the text color for the footer
+    // document.getElementById("websiteFooter").style.color = textColorValue;
+
+    // Update the text color for the footer
+    var footerElements = document.querySelectorAll('#websiteFooter *');
+    // var footerElements = document.querySelectorAll('#websiteFooter .social-media-handle, #websiteFooter .details');
+    footerElements.forEach(function(element) {
+        element.style.color = textColorValue;
     });
 
     // Save preference to Local Storage
@@ -61,17 +80,26 @@ function resetPreferences() {
     localStorage.removeItem("bgColor");
     localStorage.removeItem("textColor")
 
-    // Reset styles to default
+    // Reset Header styles to default
     document.getElementById("websiteHeader").style.fontSize = "1em";
-    document.getElementById("websiteBody").style.fontSize = "1em";
     document.getElementById("websiteHeader").style.backgroundColor = "#EAE3C9";
     document.getElementById("websiteHeader").style.color = "#222222"
+    
+    // Reset Body style to default
+    document.getElementById("websiteBody").style.fontSize = "1em";
+
+    // Reset Footer styles to default
+    document.getElementById("websiteFooter").style.fontSize = "1em";
+    document.getElementById("websiteFooter").style.backgroundColor = "#EAE3C9";
+    document.getElementById("websiteFooter").style.color = "#222222"
 
     // Reset input values
     document.getElementById("fontSize").value = 1;
     document.getElementById("bgColor").value = "#EAE3C9";
     document.getElementById("textColor").value = "#222222"
 }
+///////////////////////////////////////////////////////
+
 
 //Toggle function for mobile view
 function toggleMobileMenu() {
@@ -89,3 +117,4 @@ function toggleMobileMenu() {
       timesIcon.style.display = "inline";
     }
 }
+
